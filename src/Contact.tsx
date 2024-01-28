d1cba695eb35c852143ab40f3f984276e3254660
GokulKolanthaivel <gokulsksd@gmail.com>
2024-01-28 23:45:14 +0530

    contact page and experience added

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,10 +1,67 @@
+import { Col, Row } from "react-bootstrap";
 import "./Contact.css";
 
 function Contact() {
+
   return (
-    <div className="Contact">
-     <p>Contact</p>
-    </div>
+
+    <section className="contact" id="contact">
+      <div className="container">
+        <div className="heading text-center">
+          <h2>Contact
+            <span> Us </span></h2>
+            <p>Thank you for taking the time to explore my portfolio. If you have any inquiries, <br></br>collaboration proposals, or if you simply want to connect, I would love to hear from you.<br></br> Feel free to reach out using the contact information below:</p>
+        </div>
+        <div className="row">
+          <div className="col-md-5">
+            <div className="title">
+              <h3>Contact detail</h3>
+            </div>
+            <div className="content">
+              <div className="info">
+                <i className="fas fa-mobile-alt"></i>
+                <h4 className="d-inline-block">PHONE :
+                  <br />
+                  <span>+91 8300841583</span></h4>
+              </div>
+              <div className="info">
+                <i className="far fa-envelope"></i>
+                <h4 className="d-inline-block">EMAIL :
+                  <br />
+                  <span>gokulsksd@gmail.com</span></h4>
+              </div>
+              <div className="info">
+                <i className="fas fa-map-marker-alt"></i>
+                <h4 className="d-inline-block">ADDRESS :<br />
+                  <span>73/92, Seerapalli, Rasipuram, Namakkal - 637406</span></h4>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="col-md-7">
+
+            <form>
+              <div className="row">
+                <div className="col-sm-6">
+                  <input type="text" className="form-control" placeholder="Name" />
+                </div>
+                <div className="col-sm-6">
+                  <input type="email" className="form-control" placeholder="Email" />
+                </div>
+                <div className="col-sm-12">
+                  <input type="text" className="form-control" placeholder="Subject" />
+                </div>
+              </div>
+              <div className="form-group">
+                <textarea className="form-control" id="comment" placeholder="Message"></textarea>
+              </div>
+              <button className="btn btn-block" type="submit">Send Now!</button>
+            </form>
+          </div> */}
+        </div>
+      </div>
+    </section>
+
   );
 }
 
--- src/Experience.tsx
+++ src/Experience.tsx
@@ -21,7 +21,65 @@ function Experience() {
 
   return (
     <div className="Experience">
-      <div></div>
+      <div className="experienceContainer">
+        <h2  className="companyTitle">Nallas Corporation</h2>
+          <h4 className="roleTitle">Trainee Engineer (Oct 2021 -Dec 2021)</h4>
+            <p>
+              Utilized .NET in C# to engineer web applications offering end-to-end solutions encompassing front-end and back-end services.
+            </p>
+            <p>
+              Contributed collaboratively to an internal project focused on managing employee data.
+            </p>
+            <p>
+              Played a key role in crafting tailored user interfaces that met specified criteria, while
+              seamlessly integrating backend functionalities
+            </p>
+          <h4  className="roleTitle">Associate Engineer (Jan 2022 - Mar 2023)</h4>
+            <p>
+              Orchestrated the adoption of asynchronous operations to elevate API integration
+              efficiency, optimizing the responsiveness of the system
+            </p>
+            <p>
+              Engineered Python backend enhancements through the seamless integration of
+              decorators, augmenting functionality and streamlining code execution
+            </p>
+            <p>Developed and integrated UI components utilizing ReactJS, contributing to an enriched
+              user interface and improved overall user experience
+            </p>
+            <p>Collaborated on the implementation of product-related features within the babylonJS
+              framework, tailoring functionality to meet specific project requirements and ensuring
+              seamless integration with the overall product ecosystem
+            </p>
+            <p>Addressed and rectified issues and bugs within the web application to ensure a
+              smooth and seamless user experience
+            </p>
+
+      <h2 className="companyTitle">Brahmarsive India Pvt Ltd (Sister Concern of Nallas)</h2>
+        <h4  className="roleTitle">Associate Engineer (Apr 2023 -Jan 2024)</h4>
+            <p>
+              Developed robust Python backend APIs, seamlessly connecting them with the frontend 
+              for effective communication
+            </p>
+            <p>
+              Collaborated with the design unit to craft intuitive ReactJS user interfaces, integrating 
+              immersive 3D experiences with Babylon.js
+            </p>
+            <p>
+              Engaged collaboratively to gather and seamlessly integrate system requirements across 
+              teams 
+            </p>
+            <p>
+              Expertly integrated 3D assets, executed real-time physics simulations, and enriched 
+              user interactions within Babylon.js
+            </p>
+            <p>
+              Orchestrated Azure deployment, ensuring continuous application maintenance and 
+              contributing to elevated quality standards.
+            </p>
+
+
+      </div>
+
       <div className="skillsContainer">
         <div className="skillsTitleContainer">
           <h3 title="skillsTitle">Skills</h3>
